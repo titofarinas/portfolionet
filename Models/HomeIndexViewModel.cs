43c054a30a8876ec09fa7609131b8c72ca1e921f
EdChambers <titofarinas@outlook.com>
2025-01-07 23:37:18 -0600

    Se agrega listado de proyectos

---
+++ Models/HomeIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Portfolio.Models
+{
+    public class HomeIndexViewModel
+    {
+        public Persona Persona{ get; set; }
+        public IEnumerable<Proyecto> Proyectos { get; set; }
+    }
+}
